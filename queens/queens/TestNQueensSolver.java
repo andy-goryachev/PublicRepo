5af5c3a628cd34c14761904ecae2c1b3a41c4563
Andy Goryachev <andy@goryachev.com>
2018-02-14 15:47:30 -0800

    test stub

---
+++ queens/queens/TestNQueensSolver.java
@@ -0,0 +1,16 @@
+// Copyright © 2018 Andy Goryachev <andy@goryachev.com>
+package queens;
+
+
+/**
+ * Test NQueensSolver.
+ */
+public class TestNQueensSolver
+{
+	// TODO 
+	// for N from 2 to 8(?), exhaustively generate all possible combinations, and make sure
+	// that the solver produces correct result.
+	
+	// TODO
+	// pick a large N (16?) and measure the time, possibly comparing it to a trivial implementation
+}
